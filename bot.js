e94b12d12f206cc4454ad005602f770d17c5a81b
frog <neilsemail@me.com>
2019-01-20 20:43:52 -0500

    Simplified

--- bot.js
+++ bot.js
@@ -1,17 +1,9 @@
-var Updater = require('./updater.js');
 var Twitter = require('twitter');
-var axios = require('axios')
-var nodeCleanup = require('node-cleanup');
-var json = require('jsonfile');
 var clientKeychain = json.readFileSync('../codes/cuckwatch/code.json');
 
 class CuckWatch {
 
 	constructor() {
-		this.updater = new Updater();
-		this.updater.name = "cuckwatch";
-		this.updater.desc = "Cuck Retweeter.";
-		this.storage = json.readFileSync('./storage.json');
 		this.client = new Twitter(clientKeychain);;
 		this.stream;
 
@@ -32,10 +24,8 @@ class CuckWatch {
 	retweetTweet(id, username) {
 		this.client.post('statuses/retweet', {id: id}, (error, tweet, response) => {
 		  if(error) {
-		  	console.log('Encountered an error while retweeting.');
-		 	console.log(error)
+		  	console.log(`Encountered error while retweeting a tweet. ${error}`)
 		  } else {
-			this.storage.cuckCount+=1;
 			console.log(tweet.text);
 		  }
 		});	
@@ -46,7 +36,6 @@ class CuckWatch {
 		  if(error) {
 		  	console.log(`Encountered error while liking a tweet. ${error}`)
 		  } else {;	  
-			this.storage.favoriteCount+=1;
 			console.log(tweet.text);  
 		  }
 		});	
@@ -93,42 +82,10 @@ class CuckWatch {
 		});
 	}
 
-	update() {
- 		this.updater.post(
-			{
-				"status": 'online', 
-				"cucks-retweeted": this.storage.cuckCount, 
-				"cucks-liked": this.storage.favoriteCount,
-			})
-		.catch((e) => {
-			// console.log("Site offline or invalid URL.");
-			// silenced
-		})
-	}
-
 }
 
 
 var cuckWatch = new CuckWatch();
 cuckWatch.startStream();
 
-nodeCleanup((exitCode, signal) => {
-  if (signal) {
-	cuckWatch.stream.destroy();
-	json.writeFileSync('./storage.json', cuckWatch.storage);
-
-    cuckWatch.updater.post({'status':'offline'})
-	    .then(() => {
-	    	console.log('Posted the offline message.')
-	      	process.kill(process.pid, signal);
-	    })
-	    .catch((e) => {
-	    	console.log("Site offline or invalid url for cleanup... Whoops!");
-	      	process.kill(process.pid, signal);
-	   	})
-	  nodeCleanup.uninstall();
-	  return false;
-  }
-});
-
-//0000x00000
+//0000000x0000
